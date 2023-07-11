02835312721f6237f181b3eb8757ee157de62009
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-11 21:30:11 +0400

    feat: Implemented Requirement 1 - Chuck Card

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -7,14 +7,14 @@ interface ChuckCardProps {
 
 }
 
-const ChuckCard: React.FC<ChuckCardProps> = (props) => {
+const ChuckCard: React.FC<ChuckCardProps> = ({ greeting }) => {
 
 	return (
 		// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
 		// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
 		<>
 			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
-			<h2>{ props.greeting }</h2>
+			<h2>{ greeting }</h2>
 
 			<img src={ChuckImage} alt='A handsome man' />
 		</>
