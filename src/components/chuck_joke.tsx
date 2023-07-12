fa41ca086c1e231e008c517293abc4ec7a8e0bc9
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-12 12:00:05 +0400

    feat: Implemented Reguirement 3 - Chuck Jokes

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,8 +1,17 @@
 interface ChuckJokeProps {
+    joke: string;
 
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ joke }) => {
+
+    return (
+        <>
+           <p>{ joke }</p>
+        </>
+    );
+
+}
 
 
 
